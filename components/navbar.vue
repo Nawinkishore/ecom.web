fdb5d18ddbef698ffcf6a8b322cade7fed3e9c3c
Nawinkishore <nawinkishore2004@gmail.com>
2023-12-29 01:30:12 +0530

    Initial commit

---
+++ components/navbar.vue
@@ -0,0 +1,80 @@
+<template>
+   <div>
+    <div class="h-20 w-full shadow-lg flex items-center justify-between p-5">
+        <div>
+            <img src="https://preview.colorlib.com/theme/ashion/img/logo.png" alt="">
+        </div>
+        <div class="h-8 w-8 border-2 border-black flex justify-center items-center lg:hidden">
+            <i class="fa-solid fa-bars"
+            @click="issidebaropen=!issidebaropen"></i>
+        </div>
+        <div class="hidden lg:block">
+            <ul class="flex justify-center items-center gap-3">
+                <li v-for="(item,index) in list" :key="index">
+                    <NuxtLink :to="item.route">{{item.name}}</NuxtLink>
+                </li>
+            </ul> 
+        </div>
+        <div class="hidden lg:flex justify-center items-center gap-3">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <div class="relative">
+                <i class="fa-solid fa-heart"></i>
+                <div class="bg-black h-3 w-3 absolute -top-1 -right-1 rounded-full text-white text-[8px] flex justify-center items-center">1</div>
+            </div>
+            <div class="relative">
+                <i class="fa-solid fa-cart-shopping"></i>
+                <div class="bg-black h-3 w-3 absolute -top-1 -right-1 rounded-full text-white text-[8px] flex justify-center items-center">1</div>   
+            </div>
+        </div>
+    </div>
+    <div class="h-screen fixed top-0 w-full bg-black/80 overflow-hidden lg:hidden transition-[0.3s]"
+    :class="issidebaropen?'-left-0':'-left-full'">
+        <div class="bg-white h-screen max-w-[270px] p-5">
+            <div class="flex justify-end items-center">
+                <div class="h-7 w-7 rounded-full border border-black flex justify-center items-center p-5">
+                    <i class="fa-solid fa-xmark"
+                    @click="issidebaropen=!issidebaropen"></i>
+                </div>
+            </div>
+            <div class="flex justify-around items-center  mt-5">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <div class="relative">
+                    <i class="fa-solid fa-heart"></i>
+                    <div class="bg-black h-3 w-3 absolute -top-1 -right-1 rounded-full text-white text-[8px] flex justify-center items-center">1</div>
+                </div>
+                <div class="relative">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <div class="bg-black h-3 w-3 absolute -top-1 -right-1 rounded-full text-white text-[8px] flex justify-center items-center">1</div>   
+                </div>
+            </div>
+            <div class="mt-5">
+                <img src="https://preview.colorlib.com/theme/ashion/img/logo.png" alt="">
+            </div>
+        </div>
+    </div>
+   </div>
+    
+</template>
+<script>
+export default{
+    data(){
+        return{
+            list:[{
+                name:'Home',
+                route:'/',
+            },
+            {
+                name:'shop',
+                route:'/shop',
+            },
+            {
+                name:'Contact',
+                route:'/contact',
+            }
+            
+        ],
+        issidebaropen:false,
+        };
+    }
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  app:{
+    head:{
+      link:[
+        {rel:"stylesheet",
+        href:'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css',
+    },
+    {
+      rel:"stylesheet",
+      href:"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css",
+    },
+  ],
+  script:[
+    {
+      src:"https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"
+    },
+  ],
+    }
+  }
+});
