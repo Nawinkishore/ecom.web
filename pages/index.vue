359185424e5f60eedf4d93a5c6d1f9fd6d96a508
Nawinkishore <nawinkishore2004@gmail.com>
2023-12-29 23:58:44 +0530

    Update:index->Swiper

--- pages/index.vue
+++ pages/index.vue
@@ -81,10 +81,10 @@
               <!-- categories -->
 
               <!-- New products -->
-            <section>
+            <section class=" dark:bg-gray-900 ">
               <div  class="p-20">
-               <div class="flex justify-around items-center w-full">
-                <div><h1 class="font-bold text-2xl">New Products</h1></div>
+               <div class="flex justify-around items-center w-full text-white">
+                <div><h1 class="font-bold text-2xl ">New Products</h1></div>
                 <div>
                   <ul class="flex items-center justify-center gap-5 font-semibold text-1xl underline decoration-2 decoration-red-600 underline-offset-8">
                     <li>Women</li>
@@ -190,9 +190,9 @@
                <div class="swiper-wrapper">
                 <div v-for="n in 5" :key="n"
                 class="flex flex-col justify-center items-center swiper-slide">
-                  <h1 class="text-2xl font-medium">Get offers</h1>
-                  <p class="mt-3 font-bold text-red-600">50% offer!</p>
-                  <p class="mt-3 font-bold text-white">Shop Now</p>
+                  <h1 class="text-2xl font-medium">Best Collections</h1>
+                  <p class="mt-3 font-bold ">Get 50% offer on your first order  !</p>
+                  <p class="mt-3 font-bold underline decoration-2 decoration-red-500 underline-offset-8">Shop Now</p>
 
                 </div>
                </div>
